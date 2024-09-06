a06dfd9aa628ca4539b9b71bf8599fdf2138b140
mkheck13 <mkheck13@gmail.com>
2024-09-06 11:15:58 -0700

    convert to int lecture

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Convert to Int
+
+Console.Clear();
+
+string myTitle = "String to Int";
+Console.WriteLine(myTitle);
+
+// string breakPoint = "<------------------------New Section------------------------------->";
+
+string strNum = "2";
+int intNum = 2;
+
+Console.WriteLine(strNum + intNum);
+
+Console.WriteLine(strNum.GetType());
+Console.WriteLine(intNum.GetType());
+
+Console.WriteLine("Please enter a number: ");
+string? firstNum = Console.ReadLine();
+
+int conFirstNum = Convert.ToInt32(firstNum);
+
+Console.WriteLine(firstNum.GetType());
+Console.WriteLine(conFirstNum.GetType());
+
+Console.WriteLine(intNum + conFirstNum);
+
